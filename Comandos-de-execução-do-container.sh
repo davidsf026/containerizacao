fbb05d7a73ccd749f7f7dd62356e4a5f82c3e04a
davidsf026 <dferreira.academia@oraex.com.br>
2022-07-06 10:48:19 -0300

    Atualizando Comandos-de-execução-do-container.sh

--- Comandos-de-execução-do-container.sh
+++ Comandos-de-execução-do-container.sh
@@ -2,17 +2,27 @@
 
 ###################################### DOCKER #############################################
 # O COMANDO PARA FAZER O BUILD DO CONTAINER NO DOCKER É:
-# sudo docker build -t jboss-eap-img .
+# sudo docker build -t jboss-eap-hello-world-img .
 
 # O COMANDO PARA DAR O RUN NO CONTAINER NO DOCKER É:
-# sudo docker run -d --name jboss-eap -p 8080:8080 -p 8443:8443 -p 8778:8778 jboss-eap-img
+# sudo docker run -d --name jboss-eap-hello-world -p 8080:8080 -p 8443:8443 -p 8778:8778 jboss-eap-hello-world-img
 
 ###################################### PODMAN #############################################
 # O COMANDO PARA FAZER O BUILD DO CONTAINER NO PODMAN É:
-# sudo docker build -t jboss-eap-img .
+# sudo docker build -t jboss-eap-hello-world-img .
 
 # O COMANDO PARA DAR O RUN NO CONTAINER NO PODMAN É:
-# sudo docker run -d --name jboss-eap -p 8080:8080 -p 8443:8443 -p 8778:8778 jboss-eap-img
+# sudo docker run -d --name jboss-eap-hello-world -p 8080:8080 -p 8443:8443 -p 8778:8778 jboss-eap-hello-world-img
+
+###################################### OPENSHIFT #############################################
+# O COMANDO PARA CRIAR O PROJETO QUE IRÁ HOSPEDAR O POD/CONTAINER NO OPENSHIFT É:
+# oc new-project hello-world-project
+
+# O COMANDO PARA FAZER O BUILD/RUN DO CONTAINER NO OPENSHIFT É:
+# oc new-app --name jboss-eap-hello-world -n hello-world-project https://github.com/davidsf026/containerizacao-aplicacao/
+
+# O COMANDO PARA EXPOR UMA ROTA PARA ESSE CONTAINER É:
+# oc expose svc jboss-eap-hello-world -n hello-world-project
 
 ###################################### LEIA-ME ############################################
 # ESSE ARQUIVO É UM SHELL SCRIPT E PODE SER RODADO PARA FACILITAR A EXEMPLIFICAÇÃO, O MESMO
@@ -27,6 +37,7 @@ clear
 echo "Você quer fazer o build/run deste container no Docker ou no Podman?"
 echo "1 — Docker"
 echo "2 — Podman"
+echo "3 — OpenShift"
 
 echo 
 read -p "Escolha uma opção: " opcao
@@ -58,6 +69,26 @@ then
         echo 
         sleep 5
         podman run -d --name jboss-eap -p 8080:8080 -p 8443:8443 -p 8778:8778 jboss-eap-img
+        echo 
+elif [ $opcao == 3 ]
+then
+        echo 
+        echo -e "\033[42;1;37mRodando o comando: oc new-project hello-world-project\033[0m"
+        echo 
+        sleep 5
+        oc new-project hello-world-project
+
+        echo 
+        echo -e "\033[42;1;37mRodando o comando: oc new-app --name jboss-eap-hello-world -n hello-world-project https://github.com/davidsf026/containerizacao-aplicacao/\033[0m"
+        echo 
+        sleep 5
+        oc new-app --name jboss-eap-hello-world -n hello-world-project https://github.com/davidsf026/containerizacao-aplicacao/
+
+        echo 
+        echo -e "\033[42;1;37mRodando o comando: oc expose svc jboss-eap-hello-world -n hello-world-project\033[0m"
+        echo 
+        sleep 5
+        oc expose svc jboss-eap-hello-world -n hello-world-project
         echo 
 else
         echo 
